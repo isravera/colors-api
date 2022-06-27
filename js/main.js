741108f43486dc4214038539b28c7f0d54f4c153
Israel Vera <izrael.vb@gmail.com>
2022-06-26 20:56:04 -0400

    added cors and search in index

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-alert('working');
+document.querySelector('button').addEventListener('click', apiRequest);
+
+async function apiRequest() {
+
+    const color = document.querySelector('input').value;
+
+    if(color.length <= 0) {
+        return false;
+    }
+
+    try {
+
+        const response = await fetch(`https://koltuz-colors-api.herokuapp.com/api/${color}`);
+
+        const data = await response.json();
+
+        document.getElementById('results').innerHTML = data;
+        
+    } catch (error) {
+
+        console.error(error)        
+    }
+}
--- server.js
+++ server.js
@@ -1,47 +1,60 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = 8000;
 
+app.use(cors());
+
 const colors = {
     red: {
+        color: 'red',
         hex: '#FF0000',
         rgb: 'rgb(255, 0, 0)'
     },
     orange: {
+        color: 'orange',
         hex: '#FFA500',
         rgb: 'rgb(255, 165, 0)'
     },
     yellow: {
+        color: 'yellow',
         hex: '#FFFF00',
         rgb: 'rgb(255, 255, 0)'
     },
     purple: {
+        color: 'purple',
         hex: '#800080',
         rgb: 'rgb(128, 0, 128)'
     },
     green: {
+        color: 'green',
         hex: '#008000',
         rgb: 'rgb(0, 128, 0)'
     },
     blue: {
+        color: 'blue',
         hex: '#0000FF',
         rgb: 'rgb(0, 0, 255)'
     },
     brown: {
+        color: 'brown',
         hex: '#A52A2A',
         rgb: 'rgb(165, 42, 42)'
     },
     white: {
+        color: 'white',
         hex: '#FFFFFF',
         rgb: 'rgb(255, 255, 255)'
     },
     gray: {
+        color: 'gray',
         hex: '#808080',
         rgb: 'rgb(128, 128, 128)'
     },
     black: {
+        color: 'black',
         hex: '#000000',
         rgb: 'rgb(0, 0, 0)'
     }
@@ -54,6 +67,16 @@ const error = {
 app.get('/', (req, res) => {
 
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/js/main.js', (req, res) => {
+    
+    res.sendFile(__dirname + '/js/main.js');
+});
+
+app.get('/api', (req, res) => {
+
+    res.json(colors);
 });
 
 app.get('/api/:colorName', (req, res) => {
